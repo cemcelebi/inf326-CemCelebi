82b8501071ab3dcf3c4537cb2ed5f38ef1c0a95f
cemcelebi <cem.celebi.paul@gmail.com>
2015-12-23 16:45:54 +0200

    INF326 dersi için jumbo commit

---
+++ damacana/damacana/damacana/Controllers/ProductsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using damacana.Models;
+
+namespace damacana.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public static List<Product> CartProducts = new List<Product>{};
+
+        private ProductDBContext db = new ProductDBContext();
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult AddProduct(Product product)
+        {
+            return View(product);
+        }
+
+        public ActionResult SaveProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return View(product);
+        }
+        public ActionResult DeleteProduct(int Id)
+        {
+            foreach (Product p in db.Products)
+            {
+                if (p.Id == Id)
+                {
+                    db.Products.Remove(p);
+                    break;
+                }
+            }
+
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult EditProduct(int Id)
+        {
+            Product product = new Product();
+            foreach (Product p in db.Products)
+            {
+                if (p.Id == Id)
+                {
+                    product.Name = p.Name;
+                    product.Price = p.Price;
+                    product.Id = p.Id;
+
+                    db.Products.Remove(p);
+                    break;
+                }
+            }
+            db.SaveChanges();
+            return View(product);
+        }
+        public ActionResult ProductEdited(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return View(product);
+        }
+        public ActionResult AddtoCart(int Id)
+        {
+            Product product = new Product();
+
+            foreach (var p in db.Products)
+            {
+                if (p.Id == Id)
+                {
+                    product.Name = p.Name;
+                    product.Id = p.Id;
+                    product.Price = p.Price;
+                    CartProducts.Add(p);
+                }
+            }
+            return View(product);
+        }
+
+        public ActionResult ShowCart()
+        {
+            TempData["Cartlist"] = CartProducts;
+            return View(CartProducts);
+        }
+
+        public ActionResult DeleteFromCart(string Name)
+        {
+            foreach (Product p in CartProducts)
+            {
+                if (p.Name == Name)
+                {
+                    CartProducts.Remove(p);
+                    break;
+                }
+            }
+            return View();
+        }
+
+        public ActionResult PurchaseOrder()
+        {
+            Purchase purchase = new Purchase();
+            decimal totalprice = 0;
+            purchase.PurchaseList = new List<Product>();
+            foreach (Product p in CartProducts)
+            {
+                purchase.PurchaseList.Add(p);
+                totalprice = p.Price + totalprice;
+            }
+            purchase.TotalPrice = totalprice;
+            
+            return View(purchase);
+        }
+    }
+}
---
+++ damacana/damacana/damacana/Controllers/PurchasesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using damacana.Models;
+
+
+namespace damacana.Controllers
+{
+    public class PurchasesController : Controller
+    {
+            public User user1 = new User();
+            public PurchaseDBContext db = new PurchaseDBContext();
+            public static List<Purchase> PurchasesList = new List<Purchase>
+            {
+
+            };
+
+            public int purchaseid = 0;
+            public int i = 0;
+            public ActionResult Purchase()
+            {
+
+                Purchase mypurchase = new Purchase();
+                decimal totalprice = 0;
+                mypurchase.PurchaseList = new List<Product>();
+                foreach (Product p in (List<Product>)TempData["Cartlist"])
+                {
+                    mypurchase.PurchaseList.Add(p);
+                    totalprice = p.Price + totalprice;
+                }
+                mypurchase.Id = purchaseid;
+                purchaseid = purchaseid + 1;
+                mypurchase.TotalPrice = totalprice;
+                TempData["mypurchase"] = mypurchase;
+                i++;
+                return View(mypurchase);
+
+            }
+
+            public ActionResult PurchaseDone()
+            {
+                using (db)
+                {
+                    Purchase mypurchase = (Purchase)TempData["mypurchase"];
+                    mypurchase.User = user1;
+                    mypurchase.CreatedOn = DateTime.Now;
+                    mypurchase.UserId = i;
+                    i = i + 1;
+                    db.Purchases.Add(mypurchase);
+                    PurchasesList.Add(mypurchase);
+                    db.SaveChanges();
+                    return View(mypurchase);
+
+                }
+
+            }
+
+            public ActionResult PurchaseHistory()
+            {
+                return View(db.Purchases);
+            }
+        }
+    }
